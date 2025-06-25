700e6cc4241ff0625245069182f5f281d5d45eff
SESHAMSURYAVAMSI <suryavamsis005@gmail.com>
2025-06-25 19:03:29 +0530

    js updated

--- app/components/ProjectModal.js
+++ app/components/ProjectModal.js
@@ -5,8 +5,10 @@ export default function ProjectModal({ project, onClose, onNext, onPrev }) {
     return(
         <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center z-50">
             <div className="bg-gray-900 rounded-lg max-w-4xl w-11/12 max-h-[90vh] overflow-y-auto p-8 h-full">
-                <h2>{project.title}</h2>
+            <div>
+                <h2 className="text-2xl font-bold text-purple-50">{project.title}</h2>
                 <button onClick={onClose}></button>
+            </div>
             </div>
         </div>
     );
